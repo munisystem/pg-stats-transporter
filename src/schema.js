efe76e9152ea8f994feebc08276f597f3fd779a0
Yuichi Saito <munisystem@gmail.com>
2017-04-14 16:27:48 +0900

    Add BigQuery table schema of pg_statio_user_indexes

--- src/schema.js
+++ src/schema.js
@@ -5,7 +5,8 @@ const stats_schema = {
   "pg_stat_database": "datid:integer,datname:string,numbackends:integer,xact_commit:integer,xact_rollback:integer,blks_read:integer,blks_hit:integer,tup_returned:integer,tup_fetched:integer,tup_inserted:integer,tup_updated:integer,tup_deleted:integer,conflicts:integer,temp_files:integer,temp_bytes:integer,deadlocks:integer,blk_read_time:float,blk_write_time:float,stats_reset:timestamp",
   "pg_stat_user_tables": "relid:integer,schemaname:string,relname:integer,seq_scan:integer,seq_tup_read:integer,idx_scan:integer,idx_tup_fetch:integer,n_tup_ins:integer,n_tup_upd:integer,n_tup_del:integer,n_tup_hot_upd:integer,n_live_tup:integer,n_dead_tup:integer,n_mod_since_analyze:integer,last_vacuum:timestamp,last_autovacuum:timestamp,last_analyze:timestamp,last_autoanalyze:timestamp,vacuum_count:integer,autovacuum_count:integer,analyze_count:integer,autoanalyze_count:integer",
   "pg_stat_user_indexes": "relid:integer,indexrelid:integer,schemaname:string,relname:string,indexrelname:string,idx_scan:integer,idx_tup_read:integer,idx_tup_fetch:integer",
-  "pg_statio_user_tables": "relid:integer,schemaname:string,relname:string,heap_blks_read:integer,heap_blks_hit:integer,idx_blks_read:integer,idx_blks_hit:integer,toast_blks_read:integer,toast_blks_hit:integer,tidx_blks_read:integer,tidx_blks_hit:integer"
+  "pg_statio_user_tables": "relid:integer,schemaname:string,relname:string,heap_blks_read:integer,heap_blks_hit:integer,idx_blks_read:integer,idx_blks_hit:integer,toast_blks_read:integer,toast_blks_hit:integer,tidx_blks_read:integer,tidx_blks_hit:integer",
+  "pg_statio_user_indexes": "relid:integer,indexrelid:integer,schemaname:string,relname:string,indexrelname:string,idx_blks_read:integer,idx_blks_hit:integer"
 }
 
 module.exports = (stat) =>  {
